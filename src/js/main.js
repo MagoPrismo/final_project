1c9f226d6728a040974a4e611a4fe3ca2af0a95f
MagoPrismo <sgngms3@gmail.com>
2026-02-11 03:27:19 -0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
 import Particle from './Particles.mjs';
 
-async function init() {
-    const app = document.querySelector('#app');
+const app = document.querySelector('#app');
+const pdgIds = './json/particlesId.json';
+// we have to wait a bit 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-    const electron = new Particle('S003M');
+async function populateParticles() {
+    try {
+        const response = await fetch(pdgIds);
+        const particles = await response.json();
 
-    await electron.init();
-    app.innerHTML = electron.renderCard();
+        for (const part of particles) {
+            const p = new Particle(part.pdgId, part.name, part.category, part.src);
+            await p.fetchDetails();
+
+            const section = document.querySelector(`#${part.category}`);
+
+            section.innerHTML += p.renderCard();
+
+            await sleep(500);
+        }
+    } catch (error) {
+        console.error("Error while populating the site.")
+    }
 }
 
-init();
+populateParticles();
