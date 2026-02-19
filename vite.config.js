1bdcf6692889f6e278b22e7d752def71dce7bde2
MagoPrismo <sgngms3@gmail.com>
2026-02-19 15:58:27 -0300

    same

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: '/final_project/',
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        constants: resolve(__dirname, "src/constants.html"), // Adicione esta linha
       },
     },
   },
