dad1d1a69c7b786b42e0de29a19befad4a9d94a4
MagoPrismo <sgngms3@gmail.com>
2026-02-19 15:51:02 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,6 @@ export default defineConfig({
   base: '/final_project/',
   root: "src/",
 
-  server: {
-    proxy: {
-      // Sempre que o JS pedir algo que comece com '/pdgapi', 
-      // o Vite vai redirecionar para o servidor real da PDG.
-      '/pdgapi': {
-        target: 'https://pdgapi.lbl.gov',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/pdgapi/, '')
-      }
-    }
-  },
-
   build: {
     outDir: "../dist",
     rollupOptions: {
